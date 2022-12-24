81a8f412a26c511a2d50112578095d7537c0511c
WatcharapolxFa <watcharapolyotadee@outlook.com>
2022-12-24 11:57:50 +0700

    Success DataType

---
+++ TypeScript101/TypeData.ts
@@ -0,0 +1,8 @@
+var myName :string = "Far";
+var age : number = 29 ; 
+console.log(typeof(myName));
+console.log(typeof(age));
+
+console.log(null === undefined)
+console.log(null == undefined)
+console.log(null === null)
