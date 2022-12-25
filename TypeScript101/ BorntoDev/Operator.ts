9b6f17f603f12a1d3750e83ce3f835dfeb05dcde
WatcharapolxFa <watcharapolyotadee@outlook.com>
2022-12-25 14:49:28 +0700

    Source

---
+++ TypeScript101/ BorntoDev/Operator.ts
@@ -0,0 +1,9 @@
+let x = 5 + 4 ;
+let z = 5 - 4 ;
+let i = 5 * 4 ;
+let j = 5 / 4 ;
+
+console.log(x);
+console.log(z);
+console.log(i);
+console.log(j);
---
+++ TypeScript101/ BorntoDev/TypeData.ts
@@ -0,0 +1,27 @@
+var myName :string = "Far";
+var age : number = 29 ;
+
+const app_name = "My App " ; 
+let myArray : any[] = [1,2,3,"Hi"];
+var myName : string = "Bob";
+
+
+
+
+
+// console.log(typeof(myName));
+// console.log(typeof(age));
+
+// console.log(null === undefined);
+// console.log(null == undefined);
+// console.log(null === null);
+
+console.log(myArray);
+console.log(myArray.pop());
+console.log(myArray);
+
+console.log(myArray.push("Far"));
+console.log(myArray);
+
+let employee:[number,string,boolean]=[1,"Mister P",true];
+console.log(employee);
