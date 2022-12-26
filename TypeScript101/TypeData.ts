f4dcae259ffb0acc92ec2ad7bd33b9fb3437fa50
WatcharapolxFa <watcharapolyotadee@outlook.com>
2022-12-26 13:09:06 +0700

    commit

--- TypeScript101/TypeData.ts
+++
@@ -1,8 +0,0 @@
-var myName :string = "Far";
-var age : number = 29 ; 
-console.log(typeof(myName));
-console.log(typeof(age));
-
-console.log(null === undefined)
-console.log(null == undefined)
-console.log(null === null)
